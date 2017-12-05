cbe7676fc403579be3a300f53c5da1cd6045a591
JesusSalasMataro <jesus459@hotmail.es>
2017-12-06 00:59:42 +0100

    App

---
+++ Ajedrez/src/ajedrez/Ajedrez.java
@@ -0,0 +1,14 @@
+package ajedrez;
+
+import javax.swing.JFrame;
+
+public class Ajedrez {
+
+    public static void main(String[] args) {
+    
+        JFrame p = new PantallaPrincipal();
+        p.setVisible(true);
+        
+    }
+    
+}
